71d04460060a8a8c6c76477c66b3f85e36e90681
ColonelKai <59742334+ColonelKai@users.noreply.github.com>
2021-03-21 16:22:06 +0300

    FIX: SequencedTextDisplay in textdisplay.py 2

--- lib/textdisplay.py
+++ lib/textdisplay.py
@@ -5,7 +5,8 @@ import pygame
 #initiate font
 pygame.font.init()
 fontsize = 16
-terminalFont = pygame.font.SysFont('Monospace', fontsize)
+terminalFont = pygame.font.SysFont('Ubuntu Monospace.ttf', fontsize)
+BGCOLOR = (0,0,55)
 
 with open(os.path.join(os.getcwd(),"data/appinfo.json"), "r") as jsonfile:
 	appinfo = json.load(jsonfile)
@@ -29,14 +30,20 @@ def SequencedTextDisplay(TextDict, window, startloc, linelimit):
 	currentLine = 1
 	# the dict that will contain all of the lines - ready to be displayed.
 	textsInLines = {}
-
+	lastText = ""
 	# This is the per-line loop, it iterates through every lines text
 	for i in mainkeys:
 		# get all the states of this particular line
 		subkeys = list(TextDict[i].keys())
 		# This is for every state of a line.
 		for ii in subkeys:
-			currentText = ii
+			# calculate y coordinate of the text depending on which line it is and the font size.
+			yloc = startloc[1] + (currentLine * fontsize) - 16
+			# get the text to print
+			currentText = TextDict[str(i)][str(ii)]
+			# we will now erase the text from the previous loop.
+			wipeText = terminalFont.render(lastText, True, (BGCOLOR))
+			window.blit(wipeText, (yloc, startloc[1]))
 			# if its at the limit of the lines we can use, we will have to shift it all, eliminating the one line at the top.
 			if currentLine == linelimit:
 				# shifting all of them one up, overriding the first element of it.
@@ -52,11 +59,11 @@ def SequencedTextDisplay(TextDict, window, startloc, linelimit):
 			# this is not efficient as it displays all the text on the screen every time, but its small enough to be ignored.
 			for i in textsInLines:
 				# create the text surface, think of it as a text sprite.
-				textsurface = terminalFont.render(textsInLines[i], False, (255,255,255))
-				# calculate y coordinate of the text depending on which line it is and the font size.
-				yloc = startloc[1] + (currentLine * fontsize) - 16
+				textsurface = terminalFont.render(textsInLines[i], True, (255,255,255))
 				# blit that shit onto the screen
 				window.blit(textsurface, (yloc, startloc[1]))
+			# save the text for overriding in the next loop to allow cleanup.
+			lastText = currentText
 			# the 0.5 second delay i promised!!!
 			time.sleep(0.5)
 		# increase the line by 1 to adjust the pixel.
@@ -66,6 +73,7 @@ def SequencedTextDisplay(TextDict, window, startloc, linelimit):
 
 
 def StartupSequence(window, startloc, linelimit):
+	time.sleep(2)
 	with open(os.path.join(appinfo["parentdir"],"data/textdata/startup.json")) as file:
 		TextDict = json.load(file)
 	SequencedTextDisplay(TextDict, window, startloc, linelimit)
